2ce7b49f0e322c5430ceef3400c12dc40d7d07df
ThanhNguyenDn1 <74826340+ThanhNguyenDn1@users.noreply.github.com>
2021-08-24 17:06:06 +0700

    xem đánh giá chuyến xe

---
+++ lib/models/reviews.dart
@@ -0,0 +1,37 @@
+class Review {
+  late String tenNhaXe;
+  late String tenNd;
+  late String sdt;
+
+  late String imageUrl;
+  late int sao;
+   late String noiDungDanhGia;
+
+  Review(
+      {required this.tenNhaXe,
+        required this.tenNd,
+        required this.sdt,
+        required this.imageUrl,
+        required this.sao,
+        required this.noiDungDanhGia});
+
+  Review.fromJson(Map<String, dynamic> json) {
+    tenNhaXe = json['tenNhaXe'];
+    tenNd = json['tenNd'];
+    sdt = json['sdt'];
+    imageUrl = json['imageUrl'];
+    sao = json['sao'];
+    noiDungDanhGia = json['noiDungDanhGia'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['tenNhaXe'] = this.tenNhaXe;
+    data['tenNd'] = this.tenNd;
+    data['sdt'] = this.sdt;
+    data['imageUrl'] = this.imageUrl;
+    data['sao'] = this.sao;
+    data['noiDungDanhGia'] = this.noiDungDanhGia;
+    return data;
+  }
+}
